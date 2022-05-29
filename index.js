aa3798b35afd5849a75a231a4a0d80f44cc45199
yvonne <orumwensey@gmail.com>
2022-05-29 15:07:41 +0100

    adfded base route

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/posts", postRoute);
 app.use("/api/v1/subscribers", subscriberRoute);
 
+app.get("/", (req, res) => {
+    res.status(200).send({message: "Welcome to EMmTech"});
+})
+
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("backend srver is running")
